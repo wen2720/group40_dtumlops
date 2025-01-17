232a41f9db848d38a92e6627e983f5e9135d63c5
Justine Fastner <jfastner98@googlemail.com>
2025-01-17 15:42:02 +0100

    try new test_apis.py

--- tests/integrationtests/test_apis.py
+++ tests/integrationtests/test_apis.py
@@ -1,16 +1,31 @@
-import sys
-import os
+# import sys
+# import os
+# from fastapi.testclient import TestClient
+
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# project_root = os.path.abspath(os.path.join(current_dir, '../../'))
+# sys.path.insert(0, project_root)
+
+# from src.group40_leaf.api import app
+
+# client = TestClient(app)
+
+# def test_read_root():
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert response.json() == {"message": "Welcome to the Leaf model inference API!"}
+
+import pytest
 from fastapi.testclient import TestClient
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_dir, '../../'))
-sys.path.insert(0, project_root)
+@pytest.fixture
+def client():
+    app = TestClient()
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
-from src.group40_leaf.api import app
-
-client = TestClient(app)
-
-def test_read_root():
+def test_homepage(client):
     response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the Leaf model inference API!"}
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+    assert b"Expected content" in response.data, "Expected content not found in response"
